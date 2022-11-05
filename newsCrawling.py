4247c58e2ca92f0df5e4a6f63e062b902ab8a11b
WowCole <tda.cole@gmail.com>
2022-11-05 11:30:04 +0900

    bs

--- newsCrawling.py
+++ newsCrawling.py
@@ -1,32 +1,31 @@
 from selenium import webdriver
-import time
+
 from selenium.webdriver import Firefox
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
-import pyperclip
-import time
-import getpass
 import urllib.request
 from bs4 import BeautifulSoup
 import os
-import undetected_chromedriver as uc
-from cgitb import text
 from importlib.resources import read_text
-import pandas as pd
 import news_sites as ns
 import which_site as ws
+import requests
 
-driver = uc.Chrome()
 news_site = input()
 
 class_name=ws.find_site(news_site)
+req = requests.get(news_site)
+html=req.text
+soup = BeautifulSoup(html, 'html.parser')
+soup.figcaption.decompose()
 
+title_text = soup.select_one(
+    class_name[0]
+    ).text
 
-driver.get(news_site)
-
-title_text = driver.find_element(By.CLASS_NAME,class_name[0]).text
-
-contents_text = driver.find_element(By.CLASS_NAME,class_name[1]).text
+contents_text = soup.select_one(
+    class_name[1]
+    ).text
 
 print(title_text,contents_text)
--- news_sites.py
+++ news_sites.py
@@ -1,7 +1,5 @@
-
-
-naver={"name":"naver","title":"media_end_head_headline","contents":"_article_content"}
-daum={"name":"daum","title":"tit_view","contents":"article_view"}
+naver={"name":"naver","title":".media_end_head_headline","contents":"._article_content"}
+daum={"name":"daum","title":".tit_view","contents":".article_view"}
 
 news=[
     naver,
