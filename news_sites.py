6c04e75327fb789efc04cf461794dcf87fd7e48b
WowCole <tda.cole@gmail.com>
2022-11-06 19:16:54 +0900

    img added

--- news_sites.py
+++ news_sites.py
@@ -1,7 +1,7 @@
 
 news=[
-    {"name":"naver","title":".media_end_head_headline","contents":"._article_content"},
-    {"name":"daum","title":".tit_view","contents":".article_view"}
+    {"name":"naver","title":".media_end_head_headline","contents":"._article_content","img":"#img1"},
+    {"name":"daum","title":".tit_view","contents":".article_view","img":".thumb_g_article"}
 ]
 
 
